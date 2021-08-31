c702d4b3003c5fdcc6a2e0493ac87a54940d5119
Danail Branekov <danailster@gmail.com>
2021-08-31 14:11:58 +0000

    Support user token authentication

--- api/cloudcontroller/wrapper/k8s_auth.go
+++ api/cloudcontroller/wrapper/k8s_auth.go
@@ -16,19 +16,24 @@ import (
 	"k8s.io/client-go/transport"
 
 	"code.cloudfoundry.org/cli/api/cloudcontroller"
+	"code.cloudfoundry.org/cli/command"
 
 	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
 	authexec "k8s.io/client-go/plugin/pkg/client/auth/exec"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
 
 type K8sAuth struct {
 	connection cloudcontroller.Connection
+	config     command.Config
 }
 
-func NewK8sAuth() *K8sAuth {
-	return &K8sAuth{}
+func NewK8sAuth(config command.Config) *K8sAuth {
+	return &K8sAuth{
+		config: config,
+	}
 }
 
 func (t *K8sAuth) Make(request *cloudcontroller.Request, passedResponse *cloudcontroller.Response) error {
@@ -38,19 +43,28 @@ func (t *K8sAuth) Make(request *cloudcontroller.Request, passedResponse *cloudco
 		return err
 	}
 
-	curCtxName := conf.CurrentContext
-	if curCtxName == "" {
-		return fmt.Errorf("current context not set")
+	k8sUser := t.config.KubernetesUser()
+	if k8sUser == "" {
+		// not logged in yet
+		return nil
 	}
 
-	curCtx, ok := conf.Contexts[curCtxName]
-	if !ok {
-		return fmt.Errorf("current context not present in kube config")
+	if authInfo, ok := conf.AuthInfos[t.config.KubernetesUser()]; ok && authInfo.Token != "" {
+		return t.performUserTokenAuthentication(conf, request, passedResponse)
 	}
 
-	user, ok := conf.AuthInfos[curCtx.AuthInfo]
+	if _, ok := conf.Contexts[t.config.KubernetesUser()]; ok {
+		return t.performContextAuthentication(pathOpts, conf, request, passedResponse)
+	}
+
+	return fmt.Errorf("don't how to authenticate %q", t.config.KubernetesUser())
+}
+
+func (t *K8sAuth) performContextAuthentication(k8sConfigPathOptions *clientcmd.PathOptions, k8sConfig *clientcmdapi.Config, request *cloudcontroller.Request, passedResponse *cloudcontroller.Response) error {
+	userCtx := k8sConfig.Contexts[t.config.KubernetesUser()]
+	user, ok := k8sConfig.AuthInfos[userCtx.AuthInfo]
 	if !ok {
-		return fmt.Errorf("current user not present in kube config")
+		return fmt.Errorf("auth info %q not present for user %q in kube config", userCtx.AuthInfo, t.config.KubernetesUser())
 	}
 
 	if user.Exec != nil {
@@ -107,8 +121,8 @@ func (t *K8sAuth) Make(request *cloudcontroller.Request, passedResponse *cloudco
 	}
 
 	if user.AuthProvider != nil {
-		cluster := conf.Clusters[curCtx.Cluster].Server
-		persister := clientcmd.PersisterForUser(pathOpts, curCtx.AuthInfo)
+		cluster := k8sConfig.Clusters[userCtx.Cluster].Server
+		persister := clientcmd.PersisterForUser(k8sConfigPathOptions, userCtx.AuthInfo)
 		authProvider, err := rest.GetAuthProvider(cluster, user.AuthProvider, persister)
 		if err != nil {
 			return fmt.Errorf("could not get auth provider: %w", err)
@@ -121,6 +135,16 @@ func (t *K8sAuth) Make(request *cloudcontroller.Request, passedResponse *cloudco
 	}
 
 	return t.connection.Make(request, passedResponse)
+}
+
+func (t *K8sAuth) performUserTokenAuthentication(k8sConfig *clientcmdapi.Config, request *cloudcontroller.Request, passedResponse *cloudcontroller.Response) error {
+	authInfo := k8sConfig.AuthInfos[t.config.KubernetesUser()]
+	auth := "Bearer " + authInfo.Token
+	request.Header.Set("Authorization", auth)
+
+	connectionRoundTripper := ConnectionRoundTripper{connection: t.connection, response: passedResponse}
+	_, err := connectionRoundTripper.RoundTrip(request.Request)
+	return err
 }
 
 func (t *K8sAuth) Wrap(innerconnection cloudcontroller.Connection) cloudcontroller.Connection {
